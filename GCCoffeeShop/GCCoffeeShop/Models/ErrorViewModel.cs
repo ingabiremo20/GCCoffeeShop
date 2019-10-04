5717e738372605ee01f02e08218b1caa596d77d8
moise <ingabiremo20@gmail.com>
2019-10-04 07:57:21 -0400

    added project to the repo

---
+++ GCCoffeeShop/GCCoffeeShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GCCoffeeShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
