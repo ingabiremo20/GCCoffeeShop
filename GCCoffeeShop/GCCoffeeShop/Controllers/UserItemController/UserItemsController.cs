c1c5760d4a841687c215de7edfdcc27ce5b90516
moise <ingabiremo20@gmail.com>
2019-10-07 23:48:09 -0400

    add useritem class and it's validation

---
+++ GCCoffeeShop/GCCoffeeShop/Controllers/UserItemController/UserItemsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCCoffeeShop.Controllers.UserItemController
+{
+    public class UserItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserItemsView()
+        {
+            return View();
+        }
+    }
+}
---
+++ GCCoffeeShop/GCCoffeeShop/Models/UserItems.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GCCoffeeShop.Models
+{
+    public class UserItems
+    {
+        [Key]
+       [Required]
+        [MinLength(3)]
+        [DataType(DataType.Text)]
+        [StringLength(20, ErrorMessage = "Do not enter more than 20 characters")]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [Range(1, 150, ErrorMessage = "Please enter correct value from 1 to 150")]        
+        public float Price { get; set; }
+        [Required]
+        
+        [Range(1, 1000, ErrorMessage = "Please enter correct value from 1 to 1000")]
+        public int Quantity { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(2000, ErrorMessage = "Do not enter more than 2000 characters")]
+        public string Description { get; set; }
+    }
+}
